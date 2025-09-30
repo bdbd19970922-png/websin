f7580030665fc0ae7973bcb46434caf03ef01d99
동건 <bdbd19970922@gmail.com>
2025-09-30 15:41:23 +0900

    fix: disable SSR for HeroCarousel in (site)/page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,11 @@
-import HeroCarousel from "@/components/HeroCarousel";
+// app/(site)/page.tsx
+import dynamic from "next/dynamic";
+
+// ✅ HeroCarousel은 Swiper 사용 → SSR 비활성화
+const HeroCarousel = dynamic(() => import("@/components/HeroCarousel"), {
+  ssr: false,
+});
+
 import ServicesSummary from "@/components/ServicesSummary";
 import PortfolioPreview from "@/components/PortfolioPreview";
 import PriceSection from "@/components/PriceSection";
@@ -7,15 +14,17 @@ import GuidesTeaser from "@/components/GuidesTeaser";
 import ContactSection from "@/components/ContactSection";
 import FloatingSns from "@/components/FloatingSns";
 
-export default function Home(){
-  return (<>
-    <HeroCarousel />
-    <ServicesSummary />
-    <PortfolioPreview />
-    <PriceSection />
-    <ProcessTeaser />
-    <GuidesTeaser />
-    <ContactSection />
-    <FloatingSns />
-  </>);
+export default function SiteHome() {
+  return (
+    <>
+      <HeroCarousel />
+      <ServicesSummary />
+      <PortfolioPreview />
+      <PriceSection />
+      <ProcessTeaser />
+      <GuidesTeaser />
+      <ContactSection />
+      <FloatingSns />
+    </>
+  );
 }
